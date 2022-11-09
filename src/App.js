9dc2ab9ca38d05bd56e7042bef943982741ae30c
Elizabeth Gonzalez Cruz <elizabeth.gc091@gmail.com>
2022-11-08 20:06:19 -0600

    add main container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import ks from "./source/ks.png";
+import imageks from "./source/imageks.jpg";
 
 function App() {
   return (
@@ -8,15 +9,23 @@ function App() {
         <img id="logo-ks" src={ks} alt="logo ks" />
         <nav className="navbar">
           <ul className="list-nav">
-            <li>Inicio</li>
-            <li>Nosotros</li>
-            <li>Servicios</li>
-            <li>Cotacto</li>
+            <div className="list">
+              <li>Inicio</li>
+              <li>Nosotros</li>
+              <li>Servicios</li>
+              <li>Cotacto</li>
+            </div>
           </ul>
         </nav>
+        <div className="phone-contact">55-55-55-55-55</div>
       </header>
-      <main></main>
-      <footer></footer>
+      <main className="container-main">
+        <img id="img-bussines" src={imageks} alt="imagen negocios" />
+        <p id="text-position1">Agencia Consultora</p>
+        <p id="text-position2">Servicios Electrónicos</p>
+        <p id="text-position3">Contables - Fiscales - Nóminas</p>
+      </main>
+      <footer className="container-footer">footer</footer>
     </div>
   );
 }
